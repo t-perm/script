ac7a3c632801a9001da896ec7bbd274fe4700961
taimaiduc <taimaiduc@gmail.com>
2018-08-13 10:33:15 +0700

    install php and mysql

--- vps.sh
+++ vps.sh
@@ -20,6 +20,9 @@ show_menus() {
   echo "2. Install zsh"
   echo "3. Install zsh theme and zsh-autosuggestions"
   echo "4. Install Nginx"
+  echo "5. Install Php 7.2"
+  echo "6. Install Mysql"
+  echo "7. Install let's encrypt ssl"
   echo "quit.  Exit
   "
 }
@@ -64,6 +67,25 @@ install_nginx(){
   apt-get -y install nginx
 }
 
+install_php_72(){
+  echo -n -e "Install php7.2"
+  
+	add-apt-repository -y ppa:ondrej/php && apt-get update
+  apt-get -y install php7.2
+  apt-get -y install php7.2-fpm php7.2-curl php7.2-gd php7.2-json php7.2-mysql php7.2-sqlite3 php7.2-pgsql php7.2-bz2 php7.2-mbstring php7.2-soap php7.2-xml php7.2-zip
+  
+}
+
+install_mysql(){
+	echo -n -e "Install mysql"
+	
+	apt-get -y install mariadb-server
+	service mysql stop
+	mysql_install_db
+	service mysql start
+	mysql_secure_installation
+}
+
 # read input from the keyboard and take a action
 # invoke the one() when the user select 1 from the menu option.
 # invoke the two() when the user select 2 from the menu option.
@@ -77,6 +99,8 @@ read_options(){
     2) zsh ;;
     3) zsh_theme ;;
     4) install_nginx ;;
+    5) install_php_72 ;;
+    6) install_mysql ;;
     quit)clear && exit 0;;
     *) echo -e "${RED}Can not match with any selected${STD}" && sleep 1
   esac
