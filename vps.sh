847d7e25a4c45c227611e353dabc0cf7179589ce
taimaiduc <taimaiduc@gmail.com>
2018-08-13 11:42:29 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -95,7 +95,7 @@ install_mysql(){
 add_vhost_nginx(){
 	echo -n -e "Add  vhost nginx"
 	
-	read -p "Write the host name, eg. google:" HOST;
+	read -p "Write the host name, eg. deployer, capistrano:" HOST;
 	read -p "Write the 1st level domain name without starting dot '.', eg. com.au:" DOMAIN;
 	
 	echo "Mkdir web - logs - ssl\n"
